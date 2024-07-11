a3e8e137a6854081d29021478d752b3af6d249d9
Khayalethu_Mthethwa <khayalethumthethwa16@gmail.com>
2024-07-11 23:30:15 +0200

    Task 1 of the CodSoft internship
    This is an improvement from the first attempt and takes into consideration the ability to play again.

--- NumberGame.java
+++ NumberGame.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 
 public class NumberGame{
     public int num;
+    public Boolean playAgain;
 
     public NumberGame(){
         Random random = new Random();
         this.num = random.nextInt(100);
+        this.playAgain = true;
     }
 
     public Boolean isCorrect(int inputNum){
@@ -21,30 +23,41 @@ public class NumberGame{
             return "This number is too high";
         }
     }
+    public Boolean playAgain(int answer){
+        Random random = new Random();
+        this.num = random.nextInt(100);
+        return answer == 1;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Lets play a game, Guess a number between 1 and 100. You have 10 attempts. Goodluck!");
         NumberGame newGame = new NumberGame();
-        try (Scanner input = new Scanner(System.in)) {
-            int guessNum = input.nextInt();
-            int tracker = 0;
-            while (tracker < 10){
-                if(newGame.isCorrect(guessNum)){
-                    System.out.println("correct Number, you Win");
-                    tracker +=10;
+        try (Scanner input = new Scanner(System.in)){
+            while(newGame.playAgain){
+                System.out.println("Lets play a game, Guess a number between 1 and 100. You have 10 attempts. Goodluck!");
+                int guessNum = input.nextInt();
+                int tracker = 0;
+                while (tracker < 10){
+                    if(newGame.isCorrect(guessNum)){
+                        System.out.println("correct Number, you Win");
+                        tracker +=10;
+                    }
+                    else{
+                        System.out.println(newGame.position(guessNum));
+                        guessNum = input.nextInt();
+                        tracker+=1;
+                    }
                 }
-                else{
-                    System.out.println(newGame.position(guessNum));
-                    guessNum = input.nextInt();
-                    tracker+=1;
-                }
-            }
-            if (tracker>=10){
-                System.out.println("Game over, Correct number was: ");
+                System.out.print("Game over, Correct number was: ");
                 System.out.print(newGame.num);
-            }  
-        } catch (Exception e) {
+                System.out.println();
+                System.out.println("Do you want to play a new Game? (Type 1 = yes or 0 = no)");
+                int response = input.nextInt();
+                newGame.playAgain = newGame.playAgain(response); 
+            }
+        }catch (Exception e) {
             System.out.println("An error occured");
+            newGame.playAgain = false;
         }
+        System.out.println("Thank you for play with us!!!");
     }
 }
