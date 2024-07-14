2f44a76956c6ecd78f9b44aec75b569460548cd3
Khayalethu_Mthethwa <khayalethumthethwa16@gmail.com>
2024-07-14 19:10:53 +0200

    Upgraded Grade Calculator

--- GradeCalaulator.java
+++ GradeCalaulator.java
@@ -1,30 +1,39 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Set;
 
 public class GradeCalaulator{
-    public ArrayList<Integer> gradeBook = new ArrayList<>();
+    public HashMap<String, Integer> gradeBook;
     private int sum;
 
-    public GradeCalaulator(){}
-
-    public void addMark(int value){
-        this.gradeBook.add(value);
+    public GradeCalaulator(){
+        HashMap <String, Integer> newGradeBook = new HashMap<>();
+        this.gradeBook = newGradeBook;
     }
 
-    public void removeMark(int value){
-        int i = 0;
-        while(value!=this.gradeBook.get(i)){
-            i+=1;
-        }
-        gradeBook.remove(i+1);
+    public void addMark(String key, int grade){
+        this.gradeBook.put(key, grade);
     }
 
     public int sumOfGrades(){
-        for(int i = 0; i < gradeBook.size(); i++){
-            sum+=gradeBook.get(i);
+        sum=0;
+        if (gradeBook!=null) {
+            Set<String> e = gradeBook.keySet();
+            for (String string : e) {
+                sum+=gradeBook.get(string);
+            }
         }
         return sum;
     }
+    public void printGrades(){
+        if (gradeBook!=null) {
+            Set<String> e = gradeBook.keySet();
+            for (String string : e) {
+                System.out.printf("%-20s %s%n",string, gradeBook.get(string));
+            }
+        }
+    }
+
     public int averageGrade(){
         return sum/gradeBook.size();
     }
@@ -34,15 +43,21 @@ public class GradeCalaulator{
             System.out.println("How many subject are there");
             int noOfSubjects = scan.nextInt();
             for(int i = 0; i < noOfSubjects; i++){
-                System.out.print("Enter grade: ");
-                grades.addMark(scan.nextInt());
+                System.out.println("Enter subject name (if there are any spaces replace with a dash): ");
+                String keyInput = scan.next();
+                scan.nextLine();
+                System.out.println("Enter grade: ");
+                int gradeInput = scan.nextInt();
+                grades.addMark(keyInput, gradeInput);
             }
             int sumOfGrades = grades.sumOfGrades();
             int average = grades.averageGrade();
             System.out.println("===========================");
-            System.out.println("Total Marks: " + sumOfGrades);
-            System.out.println("Average Percentage: "+ average);
-            System.out.println("Final Grade: " + average);
+            grades.printGrades();
+            System.out.println("===========================");
+            System.out.printf("%-20s %s%n","Total Marks: ", sumOfGrades);
+            //System.out.println("Average Percentage: "+ average);
+            System.out.printf("%-20s %s%n","Final Grade: ", average);
             System.out.println("===========================");
         } catch(Exception e){
             System.out.println("An error occured");
